4025e4e56ce974f2e556f9b7092d5eab71762633
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-11 15:07:59 -0400

    add google-font and netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,10 +15,27 @@ module.export = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          'Catamaran:600,800',
+          'Roboto Mono:400,400i',
+        ]
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        langKeyDefault: 'fr',
+        useLangKeyLayout: false
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    'gatsby-plugin-netlify',
   ]
 }
