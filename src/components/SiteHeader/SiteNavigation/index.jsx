ffcc82a8933cb67d1f178ae8e85b80f6dcf1d682
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-21 19:21:18 -0400

    change SiteNavigation component to class

--- src/components/SiteHeader/SiteNavigation/index.jsx
+++ src/components/SiteHeader/SiteNavigation/index.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import Cursor from '../../Cursor';
+
+import './styles.scss';
+
 const ListItemLink = ({ to, children }) => (
   <li>
-    <Link to={to}>{children}</Link>
+    <Link to={to} className="site-navigation__link">{children}</Link>
   </li>
 )
 
-export default () => (
-  <nav>
-    <ul>
-      <ListItemLink to="/">Moi</ListItemLink>
-      <ListItemLink to="/realisations/">Réalisations</ListItemLink>
-      <ListItemLink to="/blogue/">Blogue</ListItemLink>
-      <ListItemLink to="/contact/">Contact</ListItemLink>
-    </ul>
-  </nav>
-)
+export default class SiteNavigation extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <nav className={
+        this.props.open
+          ? "site-navigation site-navigation--is-open"
+          : "site-navigation"
+        }
+      >
+        <div className="site-navigation__container">
+          <p className="site-navigation__title">Menu<Cursor flashing='true' /></p>
+
+          <ul className="site-navigation__list">
+            <ListItemLink to="/">À propos</ListItemLink>
+            <ListItemLink to="/realisations/">Réalisations</ListItemLink>
+            <ListItemLink to="/blogue/">Blogue</ListItemLink>
+            <ListItemLink to="/contact/">Contact</ListItemLink>
+          </ul>
+
+          <a href="https://www.messenger.com/t/maru3l" className="btn btn--reverse" target="_blank" rel='noopener noreferrer'>je t'attends...</a>
+        </div>
+      </nav>
+    )
+  }
+}
