69c5821813bb5f6d1425db8c8087bff52d6b9490
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-21 18:56:29 -0400

    add MenuButton to header

--- src/components/SiteHeader/index.jsx
+++ src/components/SiteHeader/index.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 
 import SiteLogo from './SiteLogo';
 import SiteNavigation from './SiteNavigation';
+import MenuButton from './MenuButton';
 
 import './styles.scss';
 
-export default () => (
-  <header className="site-header">
-    <SiteLogo />
-    <SiteNavigation />
-  </header>
-)
+export default class SiteHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuOpen: false,
+    }
+  }
+
+  render () {
+    return (
+      <header className="site-header">
+        <SiteLogo />
+        <MenuButton open={this.state.menuOpen} onClick={() => this.setState({ menuOpen: !this.state.menuOpen })} />
+        <SiteNavigation open={this.state.menuOpen} />
+      </header>
+    )
+  }
+}
