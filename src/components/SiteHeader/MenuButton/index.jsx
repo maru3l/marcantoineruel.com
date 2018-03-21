1ee7a7af89e03a248af59fffd981972106e8e0ec
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-21 18:56:13 -0400

    Create MenuButton component

---
+++ src/components/SiteHeader/MenuButton/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default props => (
+  <button
+    onClick={() => props.onClick()}
+    className={
+      props.open
+        ? 'menu-button menu-button--is-open'
+        : 'menu-button'}
+  >
+    <span className="menu-button__bread">
+      <span className="menu-button__bread-crust" />
+    </span>
+    <span className="menu-button__bread">
+      <span className="menu-button__bread-crust" />
+    </span>
+  </button>
+)
