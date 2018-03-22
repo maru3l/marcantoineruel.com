9ab5f2ac109b06a7febdf98481a0c8007d776624
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-22 16:09:21 -0400

    add resize handler to SiteHeader

--- src/components/SiteHeader/index.jsx
+++ src/components/SiteHeader/index.jsx
@@ -6,12 +6,38 @@ import MenuButton from './MenuButton';
 
 import './styles.scss';
 
+
 export default class SiteHeader extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       menuOpen: false,
+      isDesktop: false,
+    }
+  }
+
+  componentWillMount() {
+    this.handleResize()
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', () => this.handleResize()); // FIXME: Change for global window resize
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', () => this.handleResize());
+  }
+
+  handleResize() {
+    const width = window.innerWidth;
+
+    if (width < 768 && this.state.isDesktop) {
+      this.setState({ isDesktop: false });
+    }
+
+    if (width >= 768 && !this.state.isDesktop) {
+      this.setState({ isDesktop: true, menuOpen: false });
     }
   }
 
@@ -20,8 +46,11 @@ export default class SiteHeader extends React.Component {
       <header className="site-header">
         <div className="site-header__wrapper">
           <SiteLogo />
-          <MenuButton open={this.state.menuOpen} onClick={() => this.setState({ menuOpen: !this.state.menuOpen })} />
-          <SiteNavigation open={this.state.menuOpen} />
+          <SiteNavigation open={this.state.menuOpen} desktop={this.state.isDesktop} />
+          <MenuButton
+            open={this.state.menuOpen}
+            onClick={() => this.setState({ menuOpen: !this.state.menuOpen })}
+          />
         </div>
       </header>
     )
