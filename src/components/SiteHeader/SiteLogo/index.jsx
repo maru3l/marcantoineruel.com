caa2d9d2899fcae381391e71a4b352ed43762198
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-22 16:43:36 -0400

    Make name change on props long changed

--- src/components/SiteHeader/SiteLogo/index.jsx
+++ src/components/SiteHeader/SiteLogo/index.jsx
@@ -4,20 +4,9 @@ import Cursor from '../../Cursor';
 
 import './styles.scss';
 
-export default class SiteLogor extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      logo: 'Marc',
-    }
-  }
-
-  render () {
-    const content = 'Marc';
-
-    return (
-      <p className="site-logo">{content}<Cursor flashing='true' /></p>
-    )
-  }
+export default ({ long }) => {
+  const content = long ? 'Marc-Antoine Ruel' : 'Marc';
+  return(
+    <p className="site-logo">{content}<Cursor flashing='true' /></p>
+  )
 }
