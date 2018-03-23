9084516d97d15dc09a5dba79b737d51cc82fa9ac
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-23 13:33:26 -0400

    add hidding effect on scroll when window pass the top

--- src/components/SiteFooter/index.jsx
+++ src/components/SiteFooter/index.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './styles.scss';
 
-export default () => (
-  <footer className="site-footer">
-    <p className="site-footer__email-area">
-      hello[at]marcantoineruel.com
-    </p>
-    <ul className="site-footer__lang-list">
-      <li className="site-footer__lang-item">fr</li>
-      <li className="site-footer__lang-item">en</li>
-    </ul>
-  </footer>
-)
+export default class SiteFooter extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      visible: true,
+    }
+  }
+  componentDidMount() {
+    this.handleScroll();
+    window.addEventListener('scroll', () => this.handleScroll())
+  }
+
+  handleScroll() {
+    const scroll = window.scrollY;
+
+    if (scroll > 0 && this.state.visible) {
+      this.setState({ visible: false });
+    }
+
+    if (scroll < 1 && !this.state.visible) {
+      this.setState({ visible: true });
+    }
+  }
+
+  render () {
+    const siteFooterClass = classNames(
+      'site-footer',
+      {'site-footer--hidden': !this.state.visible}
+    )
+    return (
+      <footer className={siteFooterClass}>
+        <p className="site-footer__email-area">
+          hello[at]marcantoineruel.com
+        </p>
+
+        <ul className="site-footer__lang-list">
+          <li className="site-footer__lang-item">fr</li>
+          <li className="site-footer__lang-item">en</li>
+        </ul>
+      </footer>
+    )
+  };
+};
