30ea06dde48f25c5b1ad518fb779b341632c0076
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-23 12:11:58 -0400

    create SiteFooter component

---
+++ src/components/SiteFooter/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default () => (
+  <footer className="site-footer">
+    <p className="site-footer__email-area">
+      hello[at]marcantoineruel.com
+    </p>
+    <ul className="site-footer__lang-list">
+      <li className="site-footer__lang-item">fr</li>
+      <li className="site-footer__lang-item">en</li>
+    </ul>
+  </footer>
+)
