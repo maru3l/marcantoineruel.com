dc70abfcb0ef91f3992048fefc12f3e94591dd7e
Marc-Antoine Ruel <hello@marcantoineruel.com>
2018-03-24 13:19:58 -0400

    finish site container

--- src/components/Container/Header/index.jsx
+++ src/components/Container/Header/index.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import classNames from 'classnames';
 
-export default (props) => (
-  <div className="container-header">
-    <p className="container-header__title">{props.title}</p>
-  </div>
-)
+import './styles.scss';
+
+export default class ClassName extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      position: 0,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => this.handleScroll());
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', () => this.handleScroll());
+  }
+
+  handleScroll() {
+    const { innerHeight, scrollY } = window;
+
+    const delta = scrollY / innerHeight * 50;
+
+    this.setState({ position: delta });
+  }
+
+  render() {
+    const wrapperStyle = {
+      transform: `translateY(${this.state.position}vh)`
+    }
+
+    const containerHeaderClass = classNames(
+      'container-header',
+      {'container-header--is-loaded': !this.props.loading}
+    );
+
+    return (
+      <div className={containerHeaderClass} >
+        <div className="container-header__wrapper" style={wrapperStyle} >
+          <h1 className="container-header__title">{this.props.title}</h1>
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import classNames from 'classnames';
+
+import Header from './Header';
 
 import './index.scss';
 
 export default class SiteContainer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ loading: false }), 100);
   }
 
   render () {
+    const siteContainerClass = classNames(
+      'site-container',
+      {'site-container--is-loaded': !this.state.loading}
+    )
+
     return (
-      <div className="site-container">
-        {this.props.children}
+      <div className={siteContainerClass}>
+        <Header title={this.props.title} loading={this.state.loading} />
+        <div className="site-container__content">
+          <div className="site-container__skew" />
+          <div className="site-container__wrapper">{this.props.children}</div>
+        </div>
       </div>
     )
   }
